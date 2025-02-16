07d58ea7f471b8d76e0d30c4b54dc725a378454c
lihuibai7431 <2604997961@qq.com>
2025-02-16 23:24:38 +0800

    Delete 数据分析案例 directory

--- 数据分析案例/数据定义.py
+++
@@ -1,9 +0,0 @@
-#数据定义的类
-class jilu:
-    def __init__(self,data,id,money,province):
-        self.data=data
-        self.id=id
-        self.money=money
-        self.province=province
-    def __str__(self):
-        return f'{self.data},{self.id},{self.money},{self.province}'
